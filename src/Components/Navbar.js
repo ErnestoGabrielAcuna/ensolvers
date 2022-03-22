838ff489a7dce35f43ab15b8ab20522ed8aa9f33
ErnestoGabrielAcuna <gabo_allmetal@hotmail.com>
2022-03-21 19:58:42 -0500

    master

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Navbar=(brand)=>{
+    return(
+        <nav className="navbar navnar-dark bg.dark">
+            <div className="container">
+            <a href="#!" className="Navbar-bgr"> 
+            (brand)
+            </a>
+            </div>
+
+        </nav>
+    );
+
+}
+
+export default Navbar;
